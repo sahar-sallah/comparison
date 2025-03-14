7a9a7009c09034f8616a2e320cd0c54eed9fc7d5
sahar-sallah <sahar.sallah@calleebree.io>
2025-03-14 21:47:13 +0100

    backend changes

---
+++ config/middlewares.js
@@ -0,0 +1,23 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'https://natural-horse-317d4163c6.strapiapp.com',
+        'http://localhost:3000'
+      ],
+      headers: '*',
+      methods: '*'
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
