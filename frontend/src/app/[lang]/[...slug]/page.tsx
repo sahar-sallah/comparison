0986df4d70cd2564f36ffa477bb03e3ac60b43b6
sahar-sallah <sahar.sallah@calleebree.io>
2025-03-14 20:03:39 +0100

    Fix bugs

--- frontend/src/app/[lang]/[...slug]/page.tsx
+++ frontend/src/app/[lang]/[...slug]/page.tsx
@@ -27,7 +27,16 @@ export async function generateMetadata({params}: Props): Promise<Metadata> {
 
 export default async function PageRoute({params}: Props) {
     const page = await getPageBySlug(params.slug, params.lang);
-    if (page.data.length === 0) return null;
-    const contentSections = page.data[0].attributes.contentSections;
-    return contentSections.map((section: any, index: number) => componentResolver(section, index));
+
+    // Vérifier si la page a du contenu
+    if (!page.data || page.data.length === 0 || !page.data[0].attributes?.contentSections) {
+        return null;
+    }
+
+    const contentSections = page.data[0].attributes.contentSections || [];
+
+    return contentSections.map((section: any, index: number) =>
+        componentResolver(section, index)
+    );
 }
+
